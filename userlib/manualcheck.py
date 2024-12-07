3e39e573aa0aa4814ffc1b1bebac2efa22927ab2
mccodeai <mbadesert@gmail.com>
2024-12-07 23:34:59 +0800

    kg

--- userlib/manualcheck.py
+++ userlib/manualcheck.py
@@ -2,3 +2,6 @@ def mc(check):
     if check == 1:
         user_input = input("Press Enter to continue")  # Wait for user input
     return user_input
+
+y = 1 # if manual check waiting for debugger's input
+n =0 # if manual check not waiting for debugger's input
--- userlib/user_input.py
+++ userlib/user_input.py
@@ -4,5 +4,3 @@ game_keywords = "黑神话"
 
 # User variable
 log_verbose = False
-y = 1 # if manual check waiting for debugger's input
-n =0 # if manual check not waiting for debugger's input
